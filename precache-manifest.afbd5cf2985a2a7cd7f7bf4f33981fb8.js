7a2061cb7a23784656a1433c78d157a4147643da
Marcel Puetz <marcel.puetz@netlight.com>
2020-01-28 11:17:03 +0100

    Updates

---
+++ precache-manifest.afbd5cf2985a2a7cd7f7bf4f33981fb8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3abdc75a93c10e294bd0f2cb17c0934a",
+    "url": "/react-station/index.html"
+  },
+  {
+    "revision": "173c2ea8057c7c3dc8b6",
+    "url": "/react-station/static/js/2.e4d8d3c1.chunk.js"
+  },
+  {
+    "revision": "0bf02b0b0d101ee2ccf3",
+    "url": "/react-station/static/js/main.16b16d76.chunk.js"
+  },
+  {
+    "revision": "abe755fd1173ee90bb93",
+    "url": "/react-station/static/js/runtime-main.f0f8c642.js"
+  }
+]);
