c1e75d8c5f3a361ba2a50f33cf688a76d6bcf3eb
Marcel Puetz <marcel.puetz@netlight.com>
2020-01-28 12:02:27 +0100

    Updates

---
+++ precache-manifest.a3cd066a3f0c238305c48faa95368f3e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "019148d2806d06e828cece681f56929c",
+    "url": "/react-station/index.html"
+  },
+  {
+    "revision": "0e405754e74952741799",
+    "url": "/react-station/static/js/2.8e69f504.chunk.js"
+  },
+  {
+    "revision": "7edeabcf433beed4c623",
+    "url": "/react-station/static/js/main.795a58c5.chunk.js"
+  },
+  {
+    "revision": "abe755fd1173ee90bb93",
+    "url": "/react-station/static/js/runtime-main.f0f8c642.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-station/precache-manifest.72cba40ef8089d083dd1cd685acbd552.js"
+  "/react-station/precache-manifest.a3cd066a3f0c238305c48faa95368f3e.js"
 );
 
 self.addEventListener('message', (event) => {
